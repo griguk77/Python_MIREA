d6eb0c34965135ed1ff60c0c012c933be03c5c7c
griguk77 <73351839+griguk77@users.noreply.github.com>
2022-02-23 19:37:35 +0300

    Update 2.py

--- PR1/2.py
+++ PR1/2.py
@@ -1,3 +1,23 @@
+x1 = 42
+x2 = 42.0
+x3 = int(42.0)
+x4 = float(42.0)
+x5 = 0x2A
+x6 = 0b101010
+x7 = 0o52
+x8 = bin(42)
+x9 = oct(42)
+x10 = hex(42)
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+print(x5)
+print(x6)
+print(x7)
+print(x8)
+print(x9)
+print(x10)
 i = 100000000000000000000000000
 while(i>0):
     i = i ** i
