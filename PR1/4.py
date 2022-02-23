fa53a1885b834eac8eed7315768db26ece4b9dbd
griguk77 <73351839+griguk77@users.noreply.github.com>
2022-02-23 23:00:15 +0300

    Update 4.py

--- PR1/4.py
+++ PR1/4.py
@@ -14,3 +14,28 @@ def sixteen(x):
     return x
 a = int (input("Введите число: "))
 print(sixteen(a))
+
+def fifteen(x):
+    y = x
+    x = x + x
+    x = x + x
+    x = x + x
+    y = y - x
+    x = x - y
+    return x
+a = int (input("Введите число: "))
+print(fifteen(a))
+
+def twenty_nine(x):
+    y = x
+    x = x + x
+    x = x + x
+    z = x
+    x = x + x
+    x = x + x
+    x = x + x
+    x = x + y
+    x = x - z
+    return x
+a = int (input("Введите число: "))
+print(twenty_nine(a))
